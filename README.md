a90edd68f3fca9a01bcc214f6f7935948b61a6f2
lillieayer <lillieayer@gmail.com>
2024-09-03 14:35:12 -0400

    added requirements folder for pillow library, added in iteration for finding leafs, and first draft for drawing on screenshots, still need to fix isolating bounds coordinates

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# cs435assignment1
+# CS435 Assignment 1
+
+## Installation
+### 1. In order to compile and run code first clone the repository into your editor and cd into the src folder of the project
+```bash
+   git clone https://github.com/yourusername/yourproject.git
+   cd yourproject/src
+```
+### 2. Download all dependencies by running
+
+```bash
+ pip install -r requirements.txt
+```
+## Running the Tool
+
+#### All of the given filenames are hardcoded into the main script into an array called given_filenames. If you would like to add more filenames be sure to do so.
+1. Be sure to download the Programming-Assignment-Data folder with all of the xml and png files onto your computer. Add the file path of this folder into the script where denoted by the variable name folderLocation.
+
--- src/main.py
+++ src/main.py
@@ -1,13 +1,38 @@
-import xml.etree.ElementTree as ET
-from xml.dom.minidom import parse, parseString
-import sys, os
 
-# have all xml files in the same folder and input folder/folder loc
+from xml.dom.minidom import parse
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+def get_all_leafs_nodes(allNodes):
+    leaves_bounds = []
+    for node in allNodes:
+        if not node.hasChildNodes():
+            leaves_bounds.append(node)
+    return leaves_bounds
 
 if __name__ == "__main__":
-    #get current dir, folder input should just be the folder name and in same loc as this file
+    # go through provided data directory
     currdir = os.getcwd()
-    allfiles = sys.argv[1]
-    for file in os.listdir(currdir & "/" & allfiles):
+    for file in os.listdir(currdir + "/Programming-Assignment-Data"):
         if file.endswith(".xml"):
-            print(file)
+            filename = file.split(".xml")[0]
+            try:
+                # use xml parser to read xml file
+                dom = parse(file)
+                screenshot = Image.open(filename + ".png")
+                # get drawing object to write on screenshot
+                canvas = ImageDraw.Draw(screenshot)
+                allLeafs = get_all_leafs_nodes(dom.getElementsByTagName("*"))
+                for leaf in allLeafs:
+                    # get coordinates for leaf components
+                    currBound = leaf.getAttribute("bounds")
+                    # format into just #
+                    currBound = currBound.split("[")
+                    print(currBound)
+                    canvas.rectangle(((x0,y0), (x1,y1)), outline="yellow", width=3)
+                    # save in output folder
+                    screenshot.save(currdir + "/output/annotated_" + filename +".png")
+            except:
+                print("Error parsing file! please fix in file: " + file)
+            break
+'''
